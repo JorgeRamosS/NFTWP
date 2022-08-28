6f84fa3e07baa1c20b43c5706e6efc594b181424
JorgeERamosS <golub0432@yahoo.com>
2022-08-28 13:22:57 -0300

    imgs

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,4 +33,13 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add("header-bg")
+    : header.classList.remove("header-bg");
+};
+window.addEventListener("scroll", scrollHeader);
+
 /*=============== SCROLL REVEAL ANIMATION ===============*/
